9e78ed328363e65ceca5d552096adfd947f79425
steven <alvaradosteven315@gmail.com>
2023-06-03 20:42:08 -0500

    configure typeorm and postgre db connections with docker image

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+})
 export class AppModule {}
